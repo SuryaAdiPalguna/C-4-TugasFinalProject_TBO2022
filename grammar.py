0cc28d95f79c355df8b9abe2cbbfc3ca37ec3e50
SuryaAdiPalguna <100065655+SuryaAdiPalguna@users.noreply.github.com>
2022-12-18 22:49:56 +0800

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -1,3 +1,44 @@
+import general
+
+variable = ["K", "S", "P", "O", "Pel", "Ket", "NP", "VP", "AdjP", "PP", "NumP", "Verb", "Noun", "Adj", "Adv", "Num", "Prep", "PropNoun", "Pronoun"]
+production = {
+    "K" : ["SP", "KPel", "KO", "KKet"],
+    "S" : general.kata_benda + general.proper_noun + general.kata_ganti + ["NPNoun", "NPAdj", "NPPropNoun", "NPPronoun", "NumPNP"],
+    "P" : general.kata_kerja + ["AdvVP"] + general.kata_sifat + ["AdvAdjP"] + general.kata_benda + general.proper_noun + general.kata_ganti + ["NPNoun", "NPAdj", "NPPropNoun", "NPPronoun", "NumPNP"],
+    "O" : general.kata_benda + general.proper_noun + general.kata_ganti + ["NPNoun", "NPAdj", "NPPropNoun", "NPPronoun", "NumPNP"],
+    "Pel" : general.kata_benda + general.proper_noun + general.kata_ganti + ["NPNoun", "NP Adj", "NP PropNoun", "NP Pronoun", "NumP NP"] + general.kata_sifat + ["AdvAdjP"] + general.numeralia + ["NumPNum", "NumPNoun"] + general.kata_kerja + ["AdvVP"] + ["PrepNP"] + ["NPVP"],
+    "Ket" : ["PrepNP"] + general.kata_benda + general.proper_noun + general.kata_ganti + ["NPNoun", "NPAdj", "NPPropNoun", "NPPronoun", "NumPNP"],
+    "NP" : general.kata_benda + general.proper_noun + general.kata_ganti + ["NPNoun", "NPAdj", "NPPropNoun", "NPPronoun", "NumPNP"],
+    "VP" : general.kata_kerja + ["AdvVP"],
+    "AdjP" : general.kata_sifat + ["AdvAdjP"],
+    "PP" : ["PrepNP"],
+    "NumP" : general.numeralia + ["NumPNum", "NumPNoun"],
+    "Verb" : general.kata_kerja,
+    "Noun" : general.kata_benda,
+    "Adj" : general.kata_sifat,
+    "Adv" : general.kata_keterangan,
+    "Num" : general.numeralia,
+    "Prep" : general.preposisi,
+    "PropNoun" : general.proper_noun,
+    "Pronoun" : general.kata_ganti
+}
+start_symbol = ["K"]
+
+def check_production(array):
+    sum = set()
+    for i in array:
+        for j in variable:
+            if i in production.get(j):
+                sum.add(j)
+    return list(sum)
+
+def check_symbol(array):
+    for i in array:
+        if i in start_symbol:
+            return True
+    return False
+
+
 # CFG
 # ======================================================================================
 
@@ -47,4 +88,3 @@
 # AdjP → Adv AdjP | Adj
 # PP → Prep NP
 # NumP → Num | NumP Num | NumP Noun
-
