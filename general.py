60c99e5b9807657c4d1b9e71133adae1bb5c28ca
SuryaAdiPalguna <100065655+SuryaAdiPalguna@users.noreply.github.com>
2022-12-18 22:56:51 +0800

    Update general.py

--- general.py
+++ general.py
@@ -1,1 +1,39 @@
+file = open("FinalProjectTBO/file_kata/verb.txt", "r")
+kata_kerja = file.read().split("\n")
+file.close()
 
+file = open("FinalProjectTBO/file_kata/noun.txt", "r")
+kata_benda = file.read().split("\n")
+file.close()
+
+file = open("FinalProjectTBO/file_kata/adj.txt", "r")
+kata_sifat = file.read().split("\n")
+file.close()
+
+file = open("FinalProjectTBO/file_kata/adv.txt", "r")
+kata_keterangan = file.read().split("\n")
+file.close()
+
+file = open("FinalProjectTBO/file_kata/num.txt", "r")
+numeralia = file.read().split("\n")
+file.close()
+
+file = open("FinalProjectTBO/file_kata/prep.txt", "r")
+preposisi = file.read().split("\n")
+file.close()
+
+file = open("FinalProjectTBO/file_kata/prop_noun.txt", "r")
+proper_noun = file.read().split("\n")
+file.close()
+
+file = open("FinalProjectTBO/file_kata/pronoun.txt", "r")
+kata_ganti = file.read().split("\n")
+file.close()
+
+alphabet = kata_kerja + kata_benda + kata_sifat + kata_keterangan + numeralia + preposisi + proper_noun + kata_ganti
+
+def check_alphabet(array):
+    for i in array:
+        if i not in alphabet:
+            return False
+    return True
